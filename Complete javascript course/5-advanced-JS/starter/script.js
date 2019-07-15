0aa873c0357bfd31c8fc8356c3a6ca5e01013034
Tyrell Gooding <tyrell.gooding@soprabanking.com>
2019-07-15 16:31:09 +0100

    Challange 7

--- Complete javascript course/5-advanced-JS/starter/script.js
+++ Complete javascript course/5-advanced-JS/starter/script.js
@@ -291,7 +291,7 @@ function interviewQuestion(job) {
 interviewQuestion("teacher")("John");
 interviewQuestion("software developer")("Daniel");
 interviewQuestion("")("Mark");
-*/
+
 
 //////////////////////////////////////////////////
 // Bind, call and apply
@@ -370,5 +370,83 @@ king.presentation.apply(emily, ["informal queen", "morning"]);
 var kingFormal = king.presentation.bind(king, "formal king");
 kingFormal("morning");
 
-var emilyInformal = emily.presentation.bind(emily, "informal queen");
-emilyInformal("afternoon");
+
+//Challenge 7
+//Invoked Function expression.
+(function() {
+  //Function Constructor
+  function Question(question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+  }
+
+  Question.prototype.displayQuestion = function() {
+    console.log(this.question);
+    for (var i = 0; i < this.answers.length; i++) {
+      console.log(i + ": " + this.answers[i]);
+    }
+  };
+
+  Question.prototype.checkAnswer = function(ans, callBack) {
+    var sc;
+    if (ans === this.correct) {
+      console.log("Correct answer!");
+      sc = callBack(true);
+    } else ans != this.correct;
+    {
+      console.log(" Wrong answer!");
+
+      sc = callBack(false);
+    }
+
+    this.displayScore(sc);
+  };
+
+  Question.prototype.displayScore = function(score) {
+    console.log("Your current score is: " + score);
+    console.log("-------------------");
+  };
+  //Question with Array of answers.
+  var q1 = new Question(
+    " What is the capital of England?",
+    ["Paris", "London", "Lisbon"],
+    1
+  );
+
+  var q2 = new Question(
+    " What animal is the biggest?",
+    ["Elephant", "Beetle", "Tiger", "Giraffe"],
+    3
+  );
+
+  var q3 = new Question(" What is faster?", ["Car", "Speed Boat", "Jet"], 2);
+
+  var questions = [q1, q2, q3];
+
+  function score() {
+    var sc = 0;
+    return function(correct) {
+      if (correct) {
+        sc++;
+      }
+      return sc;
+    };
+  }
+
+  var keepScore = score();
+
+  function nextQuestion() {
+    var n = Math.floor(Math.random() * questions.length);
+
+    questions[n].displayQuestion();
+    var answer = prompt("Please select the correct answer.");
+
+    if (answer !== "exit") {
+      questions[n].checkAnswer(parseInt(answer), keepScore);
+      nextQuestion();
+    }
+  }
+  nextQuestion();
+})();
+*/
