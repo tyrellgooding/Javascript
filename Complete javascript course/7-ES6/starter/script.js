970d325b8068036bcf62a84b17107994215dd3b5
Tyrell Gooding <tyrell.gooding@soprabanking.com>
2019-08-05 16:20:52 +0100

    Section 8

--- Complete javascript course/7-ES6/starter/script.js
+++ Complete javascript course/7-ES6/starter/script.js
@@ -209,8 +209,7 @@ console.log(age2);
 console.log(retirement);
 */
 
-
-// Arrays 
+// Arrays
 /*
 const boxes = document.querySelectorAll('.box');
 
@@ -381,7 +380,6 @@ function GoodingPerson(firstName, yearOfBirth, lastName = 'Gooding', nationality
 var kobe = new GoodingPerson('Kobe', 1999);
 */
 
-
 //Maps
 /*
 const question = new Map();
@@ -417,3 +415,207 @@ for (let [key, value] of question.entries()) {
 const ans = parseInt(prompt('Write the correct answer:'));
 console.log(question.get(ans === question.get('Correct')));
 */
+
+//Classes
+/*
+//ES5 function constructor
+var Person5 = function (name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+}
+
+Person5.prototype.calculateAge = function () {
+  var age = new Date().getFullYear - this.yearOfBirth;
+  console.log(age);
+}
+
+var marley5 = new Person5('Marley', 1944, 'Musician');
+
+//ES6 function constructor
+class person6 {
+  constructor(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
+  calculateAge() {
+    var age = new Date().getFullYear - this.yearOfBirth;
+    console.log(age);
+  }
+
+  static greeting() {
+    console.log('Hey there!');
+  }
+
+}
+
+const brad6 = new person6('Brad', 1999, 'Quaterback');
+person6.greeting();
+
+//Classes are not hosited. mehtods can only be added to classes.
+*/
+/*
+// Inheritance
+
+//ES5 - linking 2 constructors together.
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+};
+
+Person5.prototype.calculateAge = function() {
+  var age = new Date().getFullYear() - this.yearOfBirth;
+  console.log(age);
+};
+
+var Athelete5 = function(name, yearOfBirth, job, olympicGames, medals) {
+  Person5.call(this, name, yearOfBirth, job);
+  this.olympicGames = olympicGames;
+  this.medals = medals;
+};
+
+Athelete5.prototype = Object.create(Person5.prototype);
+
+Athelete5.prototype.wonMedal = function() {
+  this.medals++;
+  console.log(this.medals);
+};
+
+var johnAthelete5 = new Athelete5("John", 1989, "swimmer", 3, 10);
+
+johnAthelete5.calculateAge();
+johnAthelete5.wonMedal();
+
+//ES6
+class person6 {
+  constructor(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
+  calculateAge() {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+  }
+}
+
+//Sub Class
+
+//extends = this class extends person6.
+class Athelete6 extends person6 {
+  constructor(name, yearOfBirth, job, olympicGames, medals) {
+    //super = calls super class.
+    super(name, yearOfBirth, job);
+    this.olympicGames = olympicGames;
+    this.medals = medals;
+  }
+  wonMedal() {
+    this.medals++;
+    console.log(this.medals);
+  }
+}
+
+const mikeAthlete6 = new Athelete6("Mike", 1978, "Sprinter", 6, 16);
+mikeAthlete6.wonMedal();
+mikeAthlete6.calculateAge();
+*/
+
+//Code Challenge
+//Element Super Class
+class Element {
+  constructor(name, buildYear) {
+    this.name = name;
+    this.buildYear = buildYear;
+  }
+}
+
+//Park Class - subclass
+class Park extends Element {
+  constructor(name, buildYear, area, numTrees) {
+    super(name, buildYear);
+    this.area = area; //km2
+    this.numTrees = numTrees;
+  }
+
+  //Method
+  treeDensity() {
+    const density = this.numTrees / this.area;
+    console.log(
+      `${this.name} has a tree density of ${density} trees per square km.`
+    );
+  }
+}
+
+class Street extends Element {
+  constructor(name, buildYear, length, size = 3) {
+    super(name, buildYear);
+    this.length = length;
+    this.size = size;
+  }
+  classifyStreet() {
+    const classification = new Map();
+    classification.set(1, "Tiny");
+    classification.set(2, "Small");
+    classification.set(3, "Normal");
+    classification.set(4, "Big");
+    classification.set(5, "Huge");
+    console.log(
+      `${this.name}, was built in ${
+        this.buildYear
+      }, and is a ${classification.get(this.size)} street.`
+    );
+  }
+}
+
+//Data/objects
+const allParks = [
+  new Park("Tusker Park", 1987, 0.2, 215),
+  new Park("Station Park", 1901, 0.5, 600),
+  new Park("Forbbiden Woods Park", 1806, 9.2, 9000)
+];
+
+const allStreets = [
+  new Street("Pickle Road", 1975, 0.9, 3),
+  new Street("Block Road", 1899, 1.2, 4),
+  new Street("Hunter Road", 1999, 0.5, 1),
+  new Street("The Long Road", 2016, 5.0)
+];
+
+function calc(arr) {
+  const sum = arr.reduce((prev, cur, index) => prev + cur, 0);
+
+  return [sum, sum / arr.length];
+}
+
+function reportParks(p) {
+  console.log("----------PARKS REPORT----------");
+  // Density
+  p.forEach(el => el.treeDensity());
+
+  // Average Age
+  const ages = p.map(el => new Date().getFullYear() - el.buildYear);
+  const [totalAge, avgAge] = calc(ages);
+  console.log(`Our ${p.length} parks have an average of ${avgAge} years.`);
+
+  //More than 1000 trees
+  const i = p.map(el => el.numTrees).findIndex(el => el >= 1000);
+  console.log(`${p[i].name} has more than 1000 trees.`);
+}
+
+function reportStreets(s) {
+  console.log("----------STREETS REPORT----------");
+
+  const [totalLength, avgLength] = calc(s.map(el => el.length));
+  console.log(
+    `Our ${
+      s.length
+    } streets have a total length of ${totalLength} km, with an average of ${avgLength} km.`
+  );
+
+  s.forEach(el => el.classifyStreet());
+}
+
+reportParks(allParks);
+reportStreets(allStreets);
