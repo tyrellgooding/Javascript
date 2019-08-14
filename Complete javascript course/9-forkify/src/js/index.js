1bfab1efffe1db7a29f9ea693be8aba2847cf861
GOODING Tyrell <tyrell.gooding@soprabanking.com>
2019-08-14 10:55:53 +0100

    1

---
+++ Complete javascript course/9-forkify/src/js/index.js
@@ -0,0 +1,4 @@
+// Global app controller
+import num from './test';
+const x = 45;
+console.log(`${num} has now been imported. Variable x is ${x}`);
---
+++ Complete javascript course/9-forkify/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: ['babel-polyfill', './src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: './dist/js/bundle.js'
+    },
+
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+
+            filename: 'index.html',
+            template: './src/index.html'
+
+        })
+    ],
+
+    module: {
+        rules: [{
+            //looks for all js files. If so babel loader will be applied.(complier)
+            test: /\.js$/,
+            //excluse this folder.
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    }
+};
