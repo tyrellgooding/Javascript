fb23f807cd773c9b9c7c4b16b857ae48600ebbab
Tyrell Gooding <tyrell.gooding@soprabanking.com>
2019-08-15 11:42:08 +0100

    render results added

--- Complete javascript course/9-forkify/src/js/index.js
+++ Complete javascript course/9-forkify/src/js/index.js
@@ -1,19 +1,40 @@
-//ES6 Modules
-/* import str from './models/Search';
-import {
-    add as a,
-    multiply as m,
-    ID
-} from './views/searchView';
-
-//import * as seacrhView from './views/searchView';
-
-console.log(`Imported functions ${a(ID, 2)} and ${m(3, 5)}. ${str}`);
-*/
+//Global Sate of the app.
+//Search Object
+//Shopping list object
+//liked recipes
 
 
 //importing package
-import Search from './models/Search'
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+import Search from "./models/Search";
+import * as searchView from './views/searchView';
+import {
+    elements
+} from './views/base';
+
+
+const state = {};
+
+const controlSearch = async () => {
+    //Get query from view.
+    const query = searchView.getInput;
+
+    if (query) {
+        //New search object and add to state.
+        state.search = new Search(query);
+
+        // prepare UI for reults.
+        searchView.clearInput();
+        searchView.clearResults();
+
+        // Search for recipies.
+        await state.search.getResults(); //<<Promise returned
+
+        // Render results on UI.
+        searchView.renderResults(state.search.result);
+    }
+};
+
+elements.searchForm.addEventListener("submit", e => {
+    e.preventDefault();
+    controlSearch();
+});
---
+++ Complete javascript course/9-forkify/src/js/views/base.js
@@ -0,0 +1,6 @@
+//Object
+export const elements = {
+    searchForm: document.querySelector(".search"),
+    searchInput: document.querySelector('.search_field'),
+    searchResList: document.querySelector('.results_list')
+};
