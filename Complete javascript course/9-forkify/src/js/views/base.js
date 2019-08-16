ab3f3f85b56f88e5a36a3e02fc36c468fa610c2b
Tyrell Gooding <tyrell.gooding@soprabanking.com>
2019-08-16 16:15:06 +0100

    Section 143

--- Complete javascript course/9-forkify/src/js/views/base.js
+++ Complete javascript course/9-forkify/src/js/views/base.js
@@ -1,6 +1,6 @@
 //Object
 export const elements = {
-    searchForm: document.querySelector(".search"),
-    searchInput: document.querySelector('.search_field'),
-    searchResList: document.querySelector('.results_list')
+    searchForm: document.querySelector('.search'),
+    searchInput: document.querySelector('.search__field'),
+    searchResList: document.querySelector('.results__list')
 };
